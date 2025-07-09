f01f69def4f65dde3ab99347debbc557eba33257
Emmanuel <emmanuel@Mac.Home>
2025-07-09 15:23:47 +0100

    changes for deployment

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,66 +1,36 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
-const mongodbUri = process.env.MONGODB_URI as string 
-const mongodbUser = process.env.MONGODB_USER as string 
+const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_DBNAME = process.env.MONGODB_DBNAME as string;
 
- 
-
-if(!mongodbUri) {
-    throw new Error ("MongoDB URI not set")
+if (!MONGODB_URI) {
+  throw new Error("MongoDB URI not set");
 }
 
+let cached = (global as any).mongoose;
 
-export default async function connectDB(){
- 
-    try{
-      await mongoose.connect(mongodbUri, {
-        dbName: mongodbUser,
-        serverSelectionTimeoutMS: 30000, 
-        socketTimeoutMS: 45000,
-    })  
-       console.log("DATABASE CONNECTION SUCCESSFUL") 
-    }catch(err){
-       console.log(err)
-    }
-   }
-   
-   
-/*if(!mongodbUser) {
-    throw new Error ("MongoDB User not set")
-}*/
-
-/*
-//cache connection
-let cached = global as typeof global & {
-    mongoose: {conn: Mongoose | null; promise: Promise<Mongoose> | null}
-} 
-
-if (!cached.mongoose) {
-    cached.mongoose = {conn: null, promise: null}
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
 }
 
-async function connectDB(): Promise<Mongoose>{
-    if(cached.mongoose.conn){
-        return cached.mongoose.conn 
-    }
-
-    if(!cached.mongoose.promise){
-        const opts = {
-            bufferCommands: false, 
-        }
-        cached.mongoose.promise = mongoose.connect(mongodbUri!, opts).then(mongoose=>mongoose)
-        
-    }
-
-    try{
-        cached.mongoose.conn = await cached.mongoose.promise;
-    }catch(err){
-        cached.mongoose.promise = null; 
-        throw err
-    }
-
-    return cached.mongoose.conn 
-} 
-
-export default connectDB;  
-*/
+export default async function connectDB() {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: MONGODB_DBNAME,
+      bufferCommands: false,
+      maxPoolSize: 10,
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      
+    }).then((mongoose) => mongoose);
+  }
+  try {
+    cached.conn = await cached.promise;
+    console.log("DATABASE CONNECTION SUCCESSFUL");
+    return cached.conn;
+  } catch (err) {
+    console.error("DATABASE CONNECTION ERROR:", err);
+    throw err;
+  }
+}

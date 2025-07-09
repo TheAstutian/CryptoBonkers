fba304c7479c3074404f41ee1093f524bbecfc5f
Emmanuel <emmanuel@Mac.Home>
2025-07-09 09:15:05 +0100

    prepp for deployment

--- src/app/fonts.tsx
+++ src/app/fonts.tsx
@@ -1,7 +1,7 @@
 import { Open_Sans, Roboto, Alegreya, Montserrat_Alternates } from "next/font/google";
 
 //fonts 
-export const headers = Montserrat_Alternates({weight: "400", subsets: [ `latin`, `latin-ext`]})
+export const headers = Montserrat_Alternates({weight: "400", subsets: [ "latin", "latin-ext"]})
 export const title = Roboto({weight: "400", subsets: ["latin", "latin-ext"]})
 export const navs = Alegreya({weight:"400", subsets: [ "latin", "latin-ext"]})
 export const weighted_title = Roboto ({weight: "variable", subsets: [ "latin", "latin-ext"]})
